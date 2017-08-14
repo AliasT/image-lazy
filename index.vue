63e861c6cb78517ed58c59c0a1d8e0b5b50fd5fc
xiaobing <1481569358@qq.com>
2017-08-14 17:17:35 +0800

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,10 +1,10 @@
 <template>
-  <img :src="img" ref="img"/>
+  <img :src="img" ref="img" @load="load($event)" alt="加载失败"/>
 </template>
 
 <script>
-  // pixel:一像素的图片
-  import pixel from '@/something/white.gif'
+  // 使用一像素的图片占位，而不用通过在dom里操作背景
+  import pixel from '@/assets/m.gif'
   export default {
     name: 'm-image',
     props: ['src', 'blur'],   
@@ -13,26 +13,18 @@
         img: this.src || pixel
       }
     },
+    methods: {
+      load(evt) {
+        console.log(evt.target)
+        const { img } = this.$refs
+        img.style.transition = "background, opacity 1s ease-out"
+        img.style.background = 'rgba(255, 255, 255, .3)'
+        evt.target.style.opacity = 1
+      }
+    },
     watch: {
       src(newValue) {
-        // 添加blur选项会给图片带来透明度动画效果
-        if(this.blur) {
-          const { img } = this.$refs
-          const _img = new Image()
-          _img.onload = (e) => {
-            // 图片下载完毕之后将原图片对象的运动效果设置如下，然后将透明度在很短的时间内重新设置为1，避免白屏晃动感
-            img.style.transition = "opacity,background, .3s ease-out"
-            img.style.background = 'rgba(255, 255, 255, .3)'
-            img.style.opacity = 0
-            setTimeout(() => {
-              img.style.opacity = '1'
-              img.src = newValue
-            }, 300)
-          }
-          _img.src = newValue
-        } else {
-          this.img = newValue
-        }
+        this.img = newValue
       }
     },
     mounted() {
@@ -40,3 +32,9 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  img {
+    opacity: 0;
+  }
+</style>
